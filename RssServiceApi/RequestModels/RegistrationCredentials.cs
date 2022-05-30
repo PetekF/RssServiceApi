4ac5bcbe6ccdb108c1517b5e9b748ded35a66440
Filip Petek <petekf@outlook.com>
2022-05-30 22:54:19 +0200

    Added RequestModels folder (and RegistrationCredentials model) to be used in request input validation and data handling

---
+++ RssServiceApi/RequestModels/RegistrationCredentials.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RssServiceApi.RequestModels
+{
+    public class RegistrationCredentials
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
