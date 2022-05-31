42147bc9a9e2fbfd5c42082865a90eb607e8a6e2
Filip Petek <petekf@outlook.com>
2022-05-31 21:16:59 +0200

    Added UNIQUE constraints to some properties of user entity

--- RssServiceApi/Entities/User.cs
+++ RssServiceApi/Entities/User.cs
@@ -1,10 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RssServiceApi.Entities
 {
+    [Index(nameof(Email), IsUnique = true)]
+    [Index(nameof(Token), IsUnique = true)]
     public class User
     {
         public int Id { get; set; }
+
         public string Email { get; set; }
         public string Password { get; set; }
         public string? Token { get; set; }
